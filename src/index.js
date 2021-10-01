5017e96bb33fe79495471c7d0077471a76a7ee41
MoralesEspina <MoralesEspina92@gmail.com>
2021-09-30 20:58:24 -0600

    Implementacion Endpoint Aeropuerto y Ruta

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.json());
 
 app.use(require('./routes/pruebas'));
+app.use(require('./routes/aeropuerto'));
+app.use(require('./routes/ruta'));
 
 app.listen(app.get('port'), () => {
     console.log(`Server on port ${app.get('port')}`);
