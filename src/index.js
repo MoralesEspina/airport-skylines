5b4a68cc5f4217622fe51c1ecb2aa7f388b39462
Dylan Alexis Tobar Orellana <dylantobar2001@gmail.com.com>
2021-09-30 18:40:04 -0600

    llamando a las routes

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.json());
 
 app.use(require('./routes/pruebas'));
+app.use(require('./routes/persona'));
+app.use(require('./routes/usuario'));
+app.use(require('./routes/rol'));
 
 app.listen(app.get('port'), () => {
     console.log(`Server on port ${app.get('port')}`);
