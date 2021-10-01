ec4b9b5aabfeb200ce7197367edb413decfae55c
Dylan Alexis Tobar Orellana <dylantobar2001@gmail.com.com>
2021-10-01 17:27:39 -0600

    arreglo de index

--- src/index.js
+++ src/index.js
@@ -5,14 +5,10 @@ app.use(express.json());
 app.set('port', process.env.PORT || 3000);
 
 
-app.use(require('./routes/persona'));
-app.use(require('./routes/usuario'));
-app.use(require('./routes/rol'));
-app.use(require('./routes/ruta'));
-app.use(require('./routes/aeropuerto'));
-app.use(require('./routes/modelo'));
-app.use(require('./routes/aerolineas'));
-app.use(require('./routes/aviones'));
+app.use(require('./routes/asiento'));
+app.use(require('./routes/boleto'));
+app.use(require('./routes/vuelo'));
+
 
 app.listen(app.get('port'), () => {
     console.log(`Server on port ${app.get('port')}`);
