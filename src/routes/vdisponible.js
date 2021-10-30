e8906236f977806da45eab6240046205b7db3a8b
DESKTOP-OHU416I\Ronald <rruanov1@miumg.edu.gt>
2021-10-30 03:46:11 -0600

    no message

--- src/routes/vdisponible.js
+++ src/routes/vdisponible.js
@@ -29,18 +29,5 @@ router.get("/disponibles", (req, res) => {
         });
 });
 
-router.post("/disponibilidad", (req, res) => {
-    console.log("Obteniendo Vuelo Disponible");
-    mysqlConnection.query('select aerolinea.nombre, ruta.origen, ruta.destino, ruta.distancia_viaje, ruta.tiempo_viaje ,ruta.precio_base ,vuelo.fecha_salida, estado_vuelo.descripcion from vuelo join ruta  join avion join aerolinea  join estado_vuelo  where ruta.id_ruta = vuelo.id_ruta  and ruta.origen = ? and ruta.destino = ? and vuelo.fecha_salida = ?',
-        [req.origen, req.destino, req.fecha_salida], (err, rows, fields) => {
-            if (!err) {
-                res.send(rows);
-            } else {
-                console.log(err);
-                res.send('error' + err);
-            }
-        });
-});
-
 
 module.exports = router;
