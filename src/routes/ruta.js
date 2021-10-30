8c102c0e679ea3a7b5e19313e1beb3616485fc6d
MoralesEspina <60724084+MoralesEspina@users.noreply.github.com>
2021-10-30 01:05:42 -0600

    Arreglo Ruta

--- src/routes/ruta.js
+++ src/routes/ruta.js
@@ -80,4 +80,19 @@ router.delete("/rutas/:id", (req, res) => {
         });
 });
 
+router.post("/disponibilidad", (req, res) => {
+    let ar = req.body;
+    console.log("Rutas");
+    console.log(ar);
+    mysqlConnection.query('select aerolinea.nombre, ruta.origen, ruta.destino, ruta.distancia_viaje, ruta.tiempo_viaje ,ruta.precio_base ,vuelo.fecha_salida, estado_vuelo.descripcion from vuelo join ruta  join avion join aerolinea  join estado_vuelo  where ruta.origen = ? and ruta.destino = ? and ruta.id_ruta = vuelo.id_ruta and avion.id_avion = vuelo.id_avion and aerolinea.id_aerolinea = avion.aerolinea and vuelo.fecha_salida = ? and estado_vuelo.id_estado = avion.estado', 
+    [ar.origen, ar.destino, ar.fecha_salida ],(err, rows, fields) => {
+        if (!err) {
+            res.send(rows);
+        } else {
+            console.log(err);
+            res.send(err);
+        }
+    });
+});
+
 module.exports = router;
