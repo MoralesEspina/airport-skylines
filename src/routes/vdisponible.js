e5e1dbb79b00e2dd5da3c2a94822562347bfc28e
DESKTOP-OHU416I\Ronald <rruanov1@miumg.edu.gt>
2021-10-30 03:43:10 -0600

    querys

--- src/routes/vdisponible.js
+++ src/routes/vdisponible.js
@@ -5,7 +5,7 @@ const mysqlConnection = require('../configurations/db-conf');
 
 router.get("/disponibles/:id_ruta", (req, res) => {
     console.log("Obteniendo Vuelo Disponible");
-    mysqlConnection.query('select z.nombre, r.origen, r.destino, r.distancia_viaje, r.tiempo_viaje ,r.precio_base ,v.fecha_hora_salida, x.descripcion from vuelo v join ruta r join avion a join aerolinea z join estado_vuelo x where r.id_ruta = v.id_ruta and a.id_avion = v.id_avion and z.id_aerolinea = a.aerolinea and x.id_estado = a.estado and v.id_ruta = ?',
+    mysqlConnection.query('select aerolinea.nombre, ruta.origen, ruta.destino, ruta.distancia_viaje, ruta.tiempo_viaje ,ruta.precio_base ,vuelo.fecha_salida, estado_vuelo.descripcion from vuelo join ruta  join avion join aerolinea  join estado_vuelo  where ruta.id_ruta = vuelo.id_ruta  and avion.id_avion = vuelo.id_avion and aerolinea.id_aerolinea = avion.aerolinea and estado_vuelo.id_estado = avion.estado and vuelo.id_ruta = ?',
         [req.params.id_ruta], (err, rows, fields) => {
             if (!err) {
                 res.send(rows);
@@ -18,8 +18,21 @@ router.get("/disponibles/:id_ruta", (req, res) => {
 
 router.get("/disponibles", (req, res) => {
     console.log("Obteniendo Vuelo Disponible");
-    mysqlConnection.query('select z.nombre, r.origen, r.destino, r.distancia_viaje, r.tiempo_viaje ,r.precio_base ,v.fecha_hora_salida, x.descripcion from vuelo v join ruta r join avion a join aerolinea z join estado_vuelo x where r.id_ruta = v.id_ruta and a.id_avion = v.id_avion and z.id_aerolinea = a.aerolinea and x.id_estado = a.estado',
+    mysqlConnection.query('select aerolinea.nombre, ruta.origen, ruta.destino, ruta.distancia_viaje, ruta.tiempo_viaje ,ruta.precio_base ,vuelo.fecha_salida, estado_vuelo.descripcion from vuelo join ruta  join avion join aerolinea  join estado_vuelo  where ruta.id_ruta = vuelo.id_ruta  and avion.id_avion = vuelo.id_avion and aerolinea.id_aerolinea = avion.aerolinea and estado_vuelo.id_estado = avion.estado',
         [req.params.id_ruta], (err, rows, fields) => {
+            if (!err) {
+                res.send(rows);
+            } else {
+                console.log(err);
+                res.send('error' + err);
+            }
+        });
+});
+
+router.post("/disponibilidad", (req, res) => {
+    console.log("Obteniendo Vuelo Disponible");
+    mysqlConnection.query('select aerolinea.nombre, ruta.origen, ruta.destino, ruta.distancia_viaje, ruta.tiempo_viaje ,ruta.precio_base ,vuelo.fecha_salida, estado_vuelo.descripcion from vuelo join ruta  join avion join aerolinea  join estado_vuelo  where ruta.id_ruta = vuelo.id_ruta  and ruta.origen = ? and ruta.destino = ? and vuelo.fecha_salida = ?',
+        [req.origen, req.destino, req.fecha_salida], (err, rows, fields) => {
             if (!err) {
                 res.send(rows);
             } else {
